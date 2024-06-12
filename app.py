37ea937449e749cf976b10f36b6b32e89410d1d7
joeyounghoon <yhjoe36511@gmail.com>
2024-06-12 21:19:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-if st.button("Home"):
+if st.button("optimalbot"):
     st.switch_page("app.py")
-if st.button("Page 1"):
-    st.switch_page("pages/page_1.py")
-if st.button("Page 2"):
-    st.switch_page("pages/page_2.py")
+if st.button("선픽"):
+    st.switch_page("pages/선픽.py")
+if st.button("후픽"):
+    st.switch_page("pages/후픽.py")
