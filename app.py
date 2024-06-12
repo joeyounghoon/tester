4966bec18308f3d8171c0ee41845cd8ad7112130
joeyounghoon <yhjoe36511@gmail.com>
2024-06-12 21:33:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 
-if st.button("optimalbot"):
-    st.switch_page("app.py")
-if st.button("선픽"):
-    st.switch_page("pages/선픽.py")
-if st.button("후픽"):
-    st.switch_page("pages/후픽.py")
+st.switch_page("app.py")
+st.switch_page("pages/선픽.py")
+st.switch_page("pages/후픽.py")
