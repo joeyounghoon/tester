ec25d081438526e1468b9cf5f61bb525a820ffca
joeyounghoon <yhjoe36511@gmail.com>
2024-06-13 12:32:03 +0900

    Update OptiMalBot.py

--- OptiMalBot.py
+++ OptiMalBot.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.image('KakaoTalk_20240612_164756569.webp', caption='리그오브레전드 바텀 조합을 확인하세요!')
-
+st.switch_page("OptiMalBot.py")
 st.switch_page("pages/선픽.py")
 st.switch_page("pages/후픽.py")
