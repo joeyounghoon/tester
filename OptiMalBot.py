f2c74419c402ed637669405224265136fdbe2a7a
joeyounghoon <yhjoe36511@gmail.com>
2024-06-13 12:26:47 +0900

    Update OptiMalBot.py

--- OptiMalBot.py
+++ OptiMalBot.py
@@ -1,5 +1,7 @@
 import streamlit as st
-st.image('sunrise.jpg', caption='Sunrise by the mountains')
+
+st.image('KakaoTalk_20240612_164756569.webp', caption='리그오브레전드 바텀 조합을 확인하세요!')
+
 st.switch_page("OptiMalBot.py")
 st.switch_page("pages/선픽.py")
 st.switch_page("pages/후픽.py")
